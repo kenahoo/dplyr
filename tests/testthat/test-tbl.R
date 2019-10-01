0f8195371c5ba52cdc033843f028a0c2074fcbd0
Ken Williams <Ken.Williams@WindLogics.com>
2019-10-01 17:04:06 -0500

    Add failing unit test

--- tests/testthat/test-tbl.R
+++ tests/testthat/test-tbl.R
@@ -14,3 +14,16 @@ test_that("tbl_vars() records groups", {
   expect_true(is_sel_vars(tbl_vars(gdf)))
   expect_identical(tbl_vars(gdf) %@% groups, c("cyl", "am"))
 })
+
+test_that("all.equal() works properly on tibbles", {
+  tibble <- dplyr::tibble
+
+  data <- tibble(a = c(0.287577520124614, 0.788305135443807),
+                 b = c(0.677570635452867, 0.572633401956409))
+
+  result <- tibble(frac = data$a / data$b)
+  expected <- tibble(frac = c(0.424424414337859, 1.37663142378798))
+
+  expect_true(all.equal(result, expected))
+  expect_equal(result, expected)
+})
